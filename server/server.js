0e12d6b69dae5e0d336127e81ef30d4c485185d3
Dormalk <dormalk@gmail.com>
2020-04-02 18:57:37 +0300

    fix routes

--- server/server.js
+++ server/server.js
@@ -11,11 +11,9 @@ var server = http.createServer(app);
 var io = socketIO(server),
     workout_connection = require('./routes/workout_connection')(io);
 
-const navigation_routes = require('./routes/navigation');
 
 
-app.use(express.static(publicPath));
-app.use('/',navigation_routes(app,express))
+app.use('/',express.static(publicPath));
 
 
 
