e55396e1769634489e0aa793a54f8ba845b8a2f9
Dormalk <dormalk@gmail.com>
2020-04-03 22:10:59 +0300

    sync code

---
+++ server/routes/upload.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const multer = require('multer');
+const path = require('path');
+
+const fileFilter = (req, files, cb) => {
+    if (files.mimetype === 'image/jpeg' || files.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+// use multer middleware
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '../../uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+var upload = multer({
+    storage: storage, limits: {
+        fileFilter: fileFilter
+    }
+}).single('file')
+
+
+const uploads = function (express) {
+    var router = express.Router();
+    
+    router.post('/', (req, res) => {
+        upload(req, res, function (err) {
+            if (err) {
+                return res.end("Error uploading file")
+            }
+
+            const file = req.file.filename;
+            console.log(file);
+            res.send(JSON.stringify(file));
+
+        });
+    });
+
+    return router;
+}
+
+module.exports = uploads;
--- server/server.js
+++ server/server.js
@@ -3,21 +3,38 @@ const publicPath = path.join(__dirname, '../public');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+const busboy = require("then-busboy");
 
 const port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server),
-    workout_connection = require('./routes/workout_connection')(io);
+workout_connection = require('./routes/workout_connection')(io);
 var navigation_routes = require('./routes/navigation');
+var uploads = require('./routes/upload');
+const login= require('./routes/login');
+
+var corsOptions ={
+  origin:'http://localhost:3000',
+     credentials: true
+};
 
 
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+app.use(cookieParser())
+app.use(express.json());
 app.use('/',express.static(publicPath));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/fileupload',uploads(express))
+app.use(login.router);
 // app.use('/',navigation_routes(app,express))
-
-
-
 
 
 
