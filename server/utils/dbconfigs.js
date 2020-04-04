c9031e2e1a4fce5d3e535d99ea30ce18ec4ab1a6
Dormalk <dormalk@gmail.com>
2020-04-04 14:13:55 +0300

    admin control fix

--- server/utils/dbconfigs.js
+++ server/utils/dbconfigs.js
@@ -2,6 +2,6 @@ module.exports = {
     host: 'us-cdbr-iron-east-01.cleardb.net',
     user: 'ba8500253e0119',
     password: 'd003fc96',
-    database: 'heroku_7a67598f03068d7',    
+    database: 'heroku_7a67598f03068d7', 
     multipleStatements: true,
 }
--- server/utils/session.js
+++ server/utils/session.js
@@ -1,15 +1,28 @@
+const PLAYING = 1;
+const PAUSE = 2;
 class Session{
+    
 
     constructor(sessionid){
         this.id = sessionid;
         this.users = []
+        this.state = PAUSE;
+        this.time = 0;
+
     }
 
     addUser(id,admin){
         var user = {id,admin};
-        if(user.admin == undefined) user.admin = true;
-        this.users.push(user);
+        if(user.admin == undefined || user.admin == '1') user.admin = true;
+        else user.admin = false;
+        if(user.admin && !this.thereIsAdmin()) this.users.push(user);
+        else if(!user.admin) this.users.push(user);
+        else return undefined;
         return user;
+    }
+
+    thereIsAdmin(){
+        return this.users.filter((user) => user.admin == true).length > 0;
     }
 
     removeUser(id){
@@ -29,6 +42,24 @@ class Session{
         var users = this.users.filter((user) => user.room === room );
         var namesArray = users.map((user) => user.id);
         return namesArray;
+    }
+
+    getState(){
+        return this.state;
+    }
+
+    getTime(){
+        return this.time;
+    }
+
+    setState(state){
+        this.state = state;
+        return this;
+    }
+
+    setTime(time){
+        this.time = time;
+        return this;
     }
 
     isEmpty(){
