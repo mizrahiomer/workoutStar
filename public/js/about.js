c6e8fcd059901b0893411a010765571e2b21daa4
Dormalk <dormalk@gmail.com>
2020-04-06 13:06:44 +0300

    about page

--- public/js/about.js
+++ public/js/about.js
@@ -5,6 +5,7 @@ const team = [
         desc: 'Product and team builder',
         link: 'michaellerner10@gmail.com',
         pic: './images/Michael.jpeg',
+        label: 'label-success'
     },
     {
         name: 'Adi Stein Ben-Nun',
@@ -12,6 +13,8 @@ const team = [
         team: 'Product Team',
         link: '#',
         pic: './images/adi.jpeg',
+        label: 'label-success'
+
     },
     {
         name: 'Dor Malka',
@@ -19,6 +22,8 @@ const team = [
         team: 'Server Team',
         link: 'dormalk@gmail.com',
         pic: 'https://avatars1.githubusercontent.com/u/30365320?s=400&u=86e3a66efca0969beab572fbea8f5ba6519fdca1&v=4',
+        label: 'label-warning'
+
     },
     {
         name: 'Omer Mizrahi',
@@ -26,6 +31,8 @@ const team = [
         desc: 'Front-end Developer',
         link: '#',
         pic: './images/omer.jpeg',
+        label: 'label-danger'
+
     },
     {
         name: 'Sharon David',
@@ -33,6 +40,8 @@ const team = [
         desc: 'UI / UX Designer',
         link: '#',
         pic: './images/sharon.jpeg',
+        label: 'label-success'
+
     },
     {
         name: 'Shaina Carlebach Schiff',
@@ -40,6 +49,8 @@ const team = [
         desc: 'Full Stack Developer, ESL coach.',
         link: 'shainaschiff@outlook.com',
         pic: './images/shaina.jpeg',
+        label: 'label-danger'
+
     }
     ,
     {
@@ -48,6 +59,8 @@ const team = [
         desc: 'Real Time Embedded Developer',
         link: '#',
         pic: './images/yaron.jpeg',
+        label: 'label-warning'
+
     }
 
 
@@ -57,8 +70,8 @@ const team = [
 
 function updateTeam(){
     var template = jQuery('#team-list').html();
-    team.forEach((({title,name, link, pic, desc}) => {
-      var html = Mustache.render(template,{title,name, link, pic, desc});
+    team.forEach((({team,name, link, pic, desc,label}) => {
+      var html = Mustache.render(template,{team,name, link, pic, desc,label});
       jQuery('#team').append(html);
     }))
 }
